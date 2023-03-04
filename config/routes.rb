d3936e83d5fd559cb92c1ecdc9d7d4fbb11adb3c
Tobias Bales <hi@tobiasbales.com>
2023-03-04 21:38:37 +0100

    feat: route root to change_detectors#index

--- config/routes.rb
+++ config/routes.rb
@@ -3,14 +3,14 @@ Rails.application.routes.draw do
   post 'sign_in', to: 'sessions#create'
   get  'sign_up', to: 'registrations#new'
   post 'sign_up', to: 'registrations#create'
-  resources :sessions, only: [:index, :show, :destroy]
-  resource  :password, only: [:edit, :update]
+  resources :sessions, only: %i[index show destroy]
+  resource  :password, only: %i[edit update]
   namespace :identity do
-    resource :email,              only: [:edit, :update]
-    resource :email_verification, only: [:show, :create]
-    resource :password_reset,     only: [:new, :edit, :create, :update]
+    resource :email,              only: %i[edit update]
+    resource :email_verification, only: %i[show create]
+    resource :password_reset,     only: %i[new edit create update]
   end
-  root 'home#index'
+  root 'change_detectors#index'
   resources :change_detectors
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
