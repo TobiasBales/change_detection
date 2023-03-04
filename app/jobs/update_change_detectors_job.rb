a339439affe470559fb7fa97bcd5c29ea7a01f81
Tobias Bales <hi@tobiasbales.com>
2023-03-04 23:22:09 +0100

    feat: send telegram notifications when a change was detected

--- app/jobs/update_change_detectors_job.rb
+++ app/jobs/update_change_detectors_job.rb
@@ -2,14 +2,34 @@ class UpdateChangeDetectorsJob < ApplicationJob
   queue_as :default
 
   def perform
+    puts('Updating change detectors')
+
     ChangeDetector.all.each do |change_detector|
+      puts("Processing change detector for #{change_detector.url} for #{change_detector.user.email}")
       response = Faraday.get(change_detector.url)
 
-      return unless response.body == change_detector.result
+      return puts('No change detected') if response.body == change_detector.result
 
-      Rails.logger.info("Change detected for #{change_detector.url}")
+      message = "Change detected for #{change_detector.url}"
+      puts(message)
+      send_telegram_message(change_detector.user.telegram_id, message)
 
       change_detector.result = response.body
+      change_detector.save!
     end
+  end
+
+  private
+
+  def telegram_secret
+    Rails.configuration.telegram_secret
+  end
+
+  def send_telegram_message(chat_id, message)
+    return puts('Not sending telegram message because no telegram secret is present') unless telegram_secret
+
+    return puts('Not sending telegram message because no chat id is present') unless chat_id
+
+    Faraday.post("https://api.telegram.org/#{telegram_secret}/sendMessage?chat_id=#{chat_id}&text=#{message}")
   end
 end
