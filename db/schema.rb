8f4d9c43363432c9f63dc053838eff66cc1b8c63
Tobias Bales <hi@tobiasbales.com>
2023-03-04 23:21:29 +0100

    feat: add telegram_id to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_04_213741) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_04_215623) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -58,6 +58,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_04_213741) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "enabled", default: false, null: false
+    t.string "telegram_id"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
