393b4f7f87fa39d7c65fb0c23bfde6c81de835ac
Tobias Bales <hi@tobiasbales.com>
2023-03-07 21:09:29 +0100

    feat: use rails logger instead of puts for update change detectors job

--- app/jobs/update_change_detectors_job.rb
+++ app/jobs/update_change_detectors_job.rb
@@ -2,19 +2,19 @@ class UpdateChangeDetectorsJob < ApplicationJob
   queue_as :default
 
   def perform
-    puts('Updating change detectors')
+    Rails.logger.info('Updating change detectors')
 
     ChangeDetector.all.each do |change_detector|
-      puts("Processing change detector for #{change_detector.url} for #{change_detector.user.email}")
+      Rails.logger.info("Processing change detector for #{change_detector.url} for #{change_detector.user.email}")
 
       change_detector.migrate_to_result_model
 
       response = Faraday.get(change_detector.url)
 
-      return puts('No change detected') unless change_detector.changed?(response.body)
+      return Rails.logger.info('No change detected') unless change_detector.changed?(response.body)
 
       message = "Change detected for #{change_detector.url}"
-      puts(message)
+      Rails.logger.info(message)
       send_telegram_message(change_detector.user.telegram_id, message)
 
       change_detector.store_result(response.body)
@@ -30,9 +30,11 @@ class UpdateChangeDetectorsJob < ApplicationJob
   end
 
   def send_telegram_message(chat_id, message)
-    return puts('Not sending telegram message because no telegram secret is present') unless telegram_secret
+    unless telegram_secret
+      return Rails.logger.info('Not sending telegram message because no telegram secret is present')
+    end
 
-    return puts('Not sending telegram message because no chat id is present') unless chat_id
+    return Rails.logger.info('Not sending telegram message because no chat id is present') unless chat_id
 
     Faraday.post("https://api.telegram.org/#{telegram_secret}/sendMessage?chat_id=#{chat_id}&text=#{message}")
   end
